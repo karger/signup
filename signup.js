f4c9abe67fd6d437d38f6bb4df056a63d4d8b462
David Karger <karger@mit.edu>
2022-07-10 02:39:08 +0300

    mostly working

--- signup.js
+++ signup.js
@@ -21,3 +21,47 @@ reTitle=function(title) {
     document.title = title.valueOf() ? title : "Event Signup";
     return title;
 }
+
+trackMyEvents = function(eventStore,uid) {
+    uid = uid.valueOf();
+    eventStore = eventStore.valueOf();
+    if (!uid || !eventStore) {
+	return;
+    }
+    firebaseReady.then((fb) => {
+	let outputNode=Mavo.all.listEvents.root.children.events;
+	let path = eventStore.split('/');
+	let query = fb.firestore();
+	while (path.length > 1) {
+	    query=query.collection(path.shift()).doc(path.shift());
+	}
+	query = query.collection(path.shift()).where('ownerId','==',uid);
+	query.onSnapshot((results) => {
+	    myEvents = [];
+	    results.forEach((doc)=> {
+		let {eventName, eventTime} = doc.data();
+		let eventId = doc.id;
+		myEvents.push({eventId: eventId, eventName: eventName,
+			       eventTime: eventTime});
+	    });
+	    outputNode.render(myEvents);
+	});
+    });
+}
+
+fbDelete = function(path) {
+    path = path.valueOf();
+    if (!path)
+	return;
+    path=path.split('/');
+    if (path.length %2) {
+	console.log("error: odd-length path (specifying a collection) passed to delete was ignored.");
+    }
+    firebaseReady.then((fb) => {
+	fb = fb.firestore();
+	while (path.length > 1) {
+	    fb = fb.collection(path.shift()).doc(path.shift());
+	}
+	fb.delete();
+    });
+}
