cc8af849488b427bc6080c9a6e35a95ada0b903f
David Karger <karger@mit.edu>
2022-07-12 17:40:39 +0300

    js set edit mode

--- signup.js
+++ signup.js
@@ -1,71 +1,72 @@
 let firebaseReady = new Promise((resolve,reject) => {
-    (function loop () {
-        if ((typeof(firebase) != "undefined") &&
-	    (firebase?.apps?.length > 0)) {
-	    resolve(firebase);
-        } else {
-	    setTimeout(loop, 100)
-        }
-    })();
+	 (function loop () {
+		  if ((typeof(firebase) != "undefined") &&
+				(firebase?.apps?.length > 0)) {
+				resolve(firebase);
+		  } else {
+				setTimeout(loop, 100)
+		  }
+	 })();
 });
 
 Promise.all([Mavo.inited,firebaseReady]).then(([m,fb])=> {
-    setTimeout(()=>{
-//	Mavo.Node.get(document.getElementById('forceEdit')).render(true);
-    }, 1000);
-
-    Mavo.all.util.dataLoaded.then(() => {
-	fb.auth().onAuthStateChanged((auth)=>{
-	    Mavo.Node.get(document.getElementById("uid")).render(auth?.uid);
-	    Mavo.Node.get(document.getElementById("name")).render(auth?.displayName);});
-    });
+	 Mavo.all.util.dataLoaded.then(() => {
+		  fb.auth().onAuthStateChanged((auth)=>{
+				Mavo.Node.get(document.getElementById("uid")).render(auth?.uid);
+				Mavo.Node.get(document.getElementById("name")).render(auth?.displayName);});
+	 });
 });
 
 reTitle=function(title) {
-    document.title = title.valueOf() ? title : "Event Signup";
-    return title;
+	 document.title = title.valueOf() ? title : "Event Signup";
+	 return title;
+}
+
+setEdit = function(on) {
+	 document.querySelector('[mv-app=event]')
+	 ?.setAttribute("mv-mode", on?.valueOf() ? "edit" : "read");
 }
 
 trackMyEvents = function(eventStore,uid) {
-    uid = uid.valueOf();
-    eventStore = eventStore.valueOf();
-    if (!uid || !eventStore) {
-	return;
-    }
-    firebaseReady.then((fb) => {
-	let outputNode=Mavo.all.listEvents.root.children.events;
-	let path = eventStore.split('/');
-	let query = fb.firestore();
-	while (path.length > 1) {
-	    query=query.collection(path.shift()).doc(path.shift());
-	}
-	query = query.collection(path.shift()).where('ownerId','==',uid);
-	query.onSnapshot((results) => {
-	    myEvents = [];
-	    results.forEach((doc)=> {
-		let {eventName, eventTime} = doc.data();
-		let eventId = doc.id;
-		myEvents.push({eventId: eventId, eventName: eventName,
-			       eventTime: eventTime});
-	    });
-	    outputNode.render(myEvents);
-	});
-    });
+	 uid = uid.valueOf();
+	 eventStore = eventStore.valueOf();
+	 if (!uid || !eventStore) {
+		  return;
+	 }
+	 firebaseReady.then((fb) => {
+		  let outputNode=Mavo.all.listEvents.root.children.events;
+		  let path = eventStore.split('/');
+		  let query = fb.firestore();
+		  while (path.length > 1) {
+				query=query.collection(path.shift()).doc(path.shift());
+		  }
+		  query = query.collection(path.shift()).where('ownerId','==',uid);
+		  query.onSnapshot((results) => {
+				myEvents = [];
+				results.forEach((doc)=> {
+					 let {eventName, eventTime} = doc.data();
+					 let eventId = doc.id;
+					 myEvents.push({eventId: eventId, eventName: eventName,
+										 eventTime: eventTime});
+				});
+				outputNode.render(myEvents);
+		  });
+	 });
 }
 
 fbDelete = function(path) {
-    path = path.valueOf();
-    if (!path)
-	return;
-    path=path.split('/');
-    if (path.length %2) {
-	console.log("error: odd-length path (specifying a collection) passed to delete was ignored.");
+	 path = path.valueOf();
+	 if (!path)
+		  return;
+	 path=path.split('/');
+	 if (path.length % 2) {
+		  console.log("error: odd-length path (specifying a collection) passed to delete was ignored.");
     }
     firebaseReady.then((fb) => {
-	fb = fb.firestore();
-	while (path.length > 1) {
-	    fb = fb.collection(path.shift()).doc(path.shift());
-	}
-	fb.delete();
+        fb = fb.firestore();
+        while (path.length >=2) {
+            fb = fb.collection(path.shift()).doc(path.shift());
+        }
+        fb.delete();
     });
 }
